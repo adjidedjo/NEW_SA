b39b43c754fcac490af9fea9016c38a38e237054
adjidedjo <adji.el.djo@gmail.com>
2017-03-31 09:04:55 +0700

    speed up checking stock

--- app/models/stock/item_availability.rb
+++ app/models/stock/item_availability.rb
@@ -4,11 +4,11 @@ class Stock::ItemAvailability < ActiveRecord::Base
   
   def self.stock_report(branch, brand)
     self.find_by_sql("SELECT IA.liitm AS liitm, SUM(IA.lipqoh) AS lipqoh, SUM(IA.lihcom) AS lihcom, 
-    IM.imlitm, IM.imdsc1, IM.imdsc2 FROM PRODDTA.F41021 IA 
+    MAX(IM.imlitm) AS imlitm, MAX(IM.imdsc1) AS imdsc1, MAX(IM.imdsc2) AS imdsc2 FROM PRODDTA.F41021 IA 
     JOIN PRODDTA.F4101 IM ON IA.liitm = IM.imitm
-    WHERE IA.limcu LIKE '%#{branch}' 
+    WHERE IA.limcu LIKE '%#{branch}' AND REGEXP_LIKE(IM.imsrp2,'KM|HB|DV|SA|SB|KB')
     AND IA.lipqoh >= 1 AND IM.imsrp1 LIKE '%#{brand}%'
-    GROUP BY IA.liitm, IM.imlitm, IM.imdsc1, IM.imdsc2")
+    GROUP BY IA.liitm, IM.imlitm")
   end
     
 end
