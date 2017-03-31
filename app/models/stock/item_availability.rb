66c19c5b617ffa1be36498596476b886f5f0bd43
adjidedjo <adji.el.djo@gmail.com>
2017-03-31 11:11:57 +0700

    add safety stock

--- app/models/stock/item_availability.rb
+++ app/models/stock/item_availability.rb
@@ -4,11 +4,14 @@ class Stock::ItemAvailability < ActiveRecord::Base
   
   def self.stock_report(branch, brand)
     self.find_by_sql("SELECT IA.liitm AS liitm, SUM(IA.lipqoh) AS lipqoh, SUM(IA.lihcom) AS lihcom, 
-    MAX(IM.imlitm) AS imlitm, MAX(IM.imdsc1) AS imdsc1, MAX(IM.imdsc2) AS imdsc2 FROM PRODDTA.F41021 IA 
+    IM.imlitm, MAX(IM.imdsc1) AS imdsc1, MAX(IM.imdsc2) AS imdsc2,
+    IB.ibsafe FROM PRODDTA.F41021 IA 
     JOIN PRODDTA.F4101 IM ON IA.liitm = IM.imitm
-    WHERE IA.limcu LIKE '%#{branch}' AND REGEXP_LIKE(IM.imsrp2,'KM|HB|DV|SA|SB|KB')
+    JOIN PRODDTA.F4102 IB ON IM.imitm = IB.ibitm
+    WHERE IB.ibmcu LIKE '%#{branch}' AND IA.limcu LIKE '%#{branch}' 
+    AND REGEXP_LIKE(IM.imsrp2,'KM|HB|DV|SA|SB|KB')
     AND IA.lipqoh >= 1 AND IM.imsrp1 LIKE '%#{brand}%'
-    GROUP BY IA.liitm, IM.imlitm")
+    GROUP BY IA.liitm, IM.imlitm, IB.ibsafe")
   end
     
 end
