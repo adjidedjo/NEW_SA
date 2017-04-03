703fc91439e7119749d7b86377b36a93d2fbdfcd
adjidedjo <adji.el.djo@gmail.com>
2017-04-03 14:40:37 +0700

    fix performance on stock

--- app/models/stock/item_availability.rb
+++ app/models/stock/item_availability.rb
@@ -3,15 +3,13 @@ class Stock::ItemAvailability < ActiveRecord::Base
   self.table_name = "proddta.f41021" #sd
   
   def self.stock_report(branch, brand)
-    self.find_by_sql("SELECT IA.liitm AS liitm, SUM(IA.lipqoh) AS lipqoh, SUM(IA.lihcom) AS lihcom, 
-    IM.imlitm, MAX(IM.imdsc1) AS imdsc1, MAX(IM.imdsc2) AS imdsc2,
-    IB.ibsafe FROM PRODDTA.F41021 IA 
+    self.find_by_sql("SELECT SUM(IA.lipqoh) AS lipqoh, SUM(IA.lihcom) AS lihcom, 
+    MAX(IM.imdsc1) AS imdsc1, MAX(IM.imdsc2) AS imdsc2 FROM PRODDTA.F41021 IA 
     JOIN PRODDTA.F4101 IM ON IA.liitm = IM.imitm
-    JOIN PRODDTA.F4102 IB ON IM.imitm = IB.ibitm
-    WHERE IB.ibmcu LIKE '%#{branch}' AND IA.limcu LIKE '%#{branch}' 
+    WHERE IA.limcu LIKE '%#{branch}' 
     AND REGEXP_LIKE(IM.imsrp2,'KM|HB|DV|SA|SB|KB')
     AND IA.lipqoh >= 1 AND IM.imsrp1 LIKE '%#{brand}%'
-    GROUP BY IA.liitm, IM.imlitm, IB.ibsafe")
+    GROUP BY IA.liitm")
   end
     
 end
