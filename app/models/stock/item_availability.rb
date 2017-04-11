ed6cbb213d3829928228889ffaa91fd60ba8d726
adjidedjo <adji.el.djo@gmail.com>
2017-04-11 11:24:03 +0700

    fix slow request on item availability

--- app/models/stock/item_availability.rb
+++ app/models/stock/item_availability.rb
@@ -3,13 +3,13 @@ class Stock::ItemAvailability < ActiveRecord::Base
   self.table_name = "proddta.f41021" #sd
   
   def self.stock_report(branch, brand)
-    self.find_by_sql("SELECT SUM(IA.lipqoh) AS lipqoh, SUM(IA.lihcom) AS lihcom, 
-    MAX(IM.imdsc1) AS imdsc1, MAX(IM.imdsc2) AS imdsc2 FROM PRODDTA.F41021 IA 
-    JOIN PRODDTA.F4101 IM ON IA.liitm = IM.imitm
-    WHERE IA.limcu LIKE '%#{branch}' 
-    AND REGEXP_LIKE(IM.imsrp2,'KM|HB|DV|SA|SB|KB')
-    AND IA.lipqoh >= 1 AND IM.imsrp1 LIKE '%#{brand}%'
-    GROUP BY IA.liitm")
+    self.find_by_sql("SELECT IA.lipqoh, IA.lihcom, IM.imdsc1, IM.imdsc2 FROM 
+    (
+     SELECT SUM(lipqoh) AS lipqoh, SUM(lihcom) AS lihcom, liitm FROM PRODDTA.F41021
+     WHERE limcu LIKE '%#{branch}' AND lipqoh >= 1 GROUP BY liitm
+    ) IA
+    LEFT OUTER JOIN PRODDTA.F4101 IM ON IA.liitm = IM.imitm WHERE 
+    REGEXP_LIKE(IM.imsrp2,'KM|HB|DV|SA|SB|KB') AND IM.imsrp1 LIKE '%#{brand}%'")
   end
     
 end
