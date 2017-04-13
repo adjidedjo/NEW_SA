198cdc61b16bf4108945604c0f8c453f5a78978e
adjidedjo <adji.el.djo@gmail.com>
2017-04-13 22:50:39 +0700

    add last updated to stock view

--- app/models/stock/item_availability.rb
+++ app/models/stock/item_availability.rb
@@ -1,15 +1,10 @@
 class Stock::ItemAvailability < ActiveRecord::Base
-  establish_connection "jdeoracle".to_sym
-  self.table_name = "proddta.f41021" #sd
+  #establish_connection "jdeoracle".to_sym
+  self.table_name = "stocks" #sd
   
   def self.stock_report(branch, brand)
-    self.find_by_sql("SELECT IA.lipqoh, IA.lihcom, IM.imdsc1, IM.imdsc2 FROM 
-    (
-     SELECT SUM(lipqoh) AS lipqoh, SUM(lihcom) AS lihcom, liitm FROM PRODDTA.F41021
-     WHERE limcu LIKE '%#{branch}' AND lipqoh >= 1 GROUP BY liitm
-    ) IA
-    LEFT OUTER JOIN PRODDTA.F4101 IM ON IA.liitm = IM.imitm WHERE 
-    REGEXP_LIKE(IM.imsrp2,'KM|HB|DV|SA|SB|KB') AND IM.imsrp1 LIKE '%#{brand}%'")
+    self.find_by_sql("SELECT onhand, available, description, updated_at FROM stocks WHERE 
+    branch = '#{branch}' AND brand = '#{brand}'")
   end
     
 end
