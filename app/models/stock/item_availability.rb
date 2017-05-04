c2e66ad64859d41d7af9a96b0b496e2179ad6094
adjidedjo <adji.el.djo@gmail.com>
2017-05-04 13:18:33 +0700

    add age to display

--- app/models/stock/item_availability.rb
+++ app/models/stock/item_availability.rb
@@ -4,12 +4,12 @@ class Stock::ItemAvailability < ActiveRecord::Base
   
   def self.stock_report(branch, brand)
     self.find_by_sql("SELECT onhand, available, buffer, description, item_number, updated_at FROM stocks 
-    WHERE branch = '#{branch}' AND brand = '#{brand}' AND onhand !=0 ")
+    WHERE branch = '#{branch}' AND brand = '#{brand}' AND onhand > 0 ")
   end
   
   def self.stock_display_report(branch, brand)
-    find_by_sql("SELECT onhand, available, description, item_number, customer, updated_at FROM display_stocks 
-    WHERE branch = '#{branch}' AND brand = '#{brand}' AND onhand !=0 GROUP BY item_number, customer")
+    find_by_sql("SELECT onhand, available, description, item_number, customer, age, updated_at FROM display_stocks 
+    WHERE branch = '#{branch}' AND brand = '#{brand}' AND onhand > 0 GROUP BY item_number, customer")
   end
     
 end
