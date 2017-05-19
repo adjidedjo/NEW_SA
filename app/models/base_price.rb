27366a7ca6b9be541175f8271b7b6d87a2322ca0
adjidedjo <adji.el.djo@gmail.com>
2017-05-19 13:12:19 +0700

    fix base prices

--- app/models/base_price.rb
+++ app/models/base_price.rb
@@ -1,19 +1,23 @@
 class BasePrice < ActiveRecord::Base
   
   def self.finding_bp_mattress(branch)
-    find_by_sql("SELECT * FROM base_prices WHERE branch IN ('#{branch}') AND product = 'KM'")
+    find_by_sql("SELECT DISTINCT item_number, description, base_price, branch, brand, product 
+    FROM base_prices WHERE branch IN ('#{branch}') AND product = 'KM'")
   end
   
   def self.finding_bp_divan(branch)
-    find_by_sql("SELECT * FROM base_prices WHERE branch IN ('#{branch}') AND product IN ('DV', 'HB')")
+    find_by_sql("SELECT DISTINCT item_number, description, base_price, branch, brand, product 
+    FROM base_prices WHERE branch IN ('#{branch}') AND product IN ('DV', 'HB')")
   end
   
   def self.finding_bp_sorong(branch)
-    find_by_sql("SELECT * FROM base_prices WHERE branch IN ('#{branch}') AND product IN ('SA', 'SB', 'ST')")
+    find_by_sql("SELECT DISTINCT item_number, description, base_price, branch, brand, product 
+    FROM base_prices WHERE branch IN ('#{branch}') AND product IN ('SA', 'SB', 'ST')")
   end
   
   def self.finding_bp_foam(branch)
-    find_by_sql("SELECT * FROM base_prices WHERE branch IN ('#{branch}') AND product = 'KB'")
+    find_by_sql("SELECT DISTINCT item_number, description, base_price, branch, brand, product 
+    FROM base_prices WHERE branch IN ('#{branch}') AND product = 'KB'")
   end
   
 end
