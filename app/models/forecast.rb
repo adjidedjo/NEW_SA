05e287f7fc0ba64914d0a5a4871fd3716ef774a2
adjidedjo <adji.el.djo@gmail.com>
2017-10-27 09:32:54 +0700

    fix upload forecast

--- app/models/forecast.rb
+++ app/models/forecast.rb
@@ -1,6 +1,6 @@
 class Forecast < ActiveRecord::Base
   def self.import(file)
-    spreadsheet = Roo::Spreadsheet.open(file.path, csv_options: {col_sep: ";"})
+    spreadsheet = Roo::Spreadsheet.open(file.path)
     header = spreadsheet.row(1)
     (2..spreadsheet.last_row).each do |i|
       row = Hash[[header, spreadsheet.row(i)].transpose]
