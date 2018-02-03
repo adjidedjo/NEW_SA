6dd263ea758a3e11cf541c23d112ba67ccd952b4
adjidedjo <adji.el.djo@gmail.com>
2018-02-03 09:58:09 +0700

    fix marketshare

--- app/models/marketshare.rb
+++ app/models/marketshare.rb
@@ -16,8 +16,9 @@ class Marketshare < ActiveRecord::Base
 
   def upcase_fields
     id = IndonesiaCity.find_by_city(self.city)
+    internal_brand = Brand.find_by_sql("SELECT * FROM brands WHERE name like '#{self.brand}'").first
+    raise internal_brand.inspect
     self.marketshare_brands.each do |bv|
-      internal_brand = Brand.find_by_name(self.brand)
       bv.area_id = id.area_id
       bv.internal_brand = self.brand
       bv.customer_name = self.customer_name
