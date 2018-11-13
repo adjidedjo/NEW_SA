03d53726e06652e3cdcba58a089f2e2b61bc6ae7
adjidedjo <adji.el.djo@gmail.com>
2018-11-13 09:03:42 +0700

    fix upcase

--- app/models/sales_productivity_customer.rb
+++ app/models/sales_productivity_customer.rb
@@ -2,10 +2,10 @@ class SalesProductivityCustomer < ActiveRecord::Base
   belongs_to :sales_productivity,  inverse_of: :sales_productivities
   
   before_create do
-    self.customer = customer.strip.upcase! if customer.present?
+    self.customer = customer.strip.upcase if customer.present?
   end
   
   before_update do
-    self.customer = customer.strip.upcase! if customer.present?
+    self.customer = customer.strip.upcase if customer.present?
   end
 end
