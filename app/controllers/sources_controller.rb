2902b69c5997f81d21cded7c21a577be60ca95d7
adjidedjo <adji.el.djo@gmail.com>
2018-12-19 08:26:06 +0700

    fix export sales reports

--- app/controllers/sources_controller.rb
+++ app/controllers/sources_controller.rb
@@ -13,7 +13,7 @@ class SourcesController < ApplicationController
   def sales_report
     @areas = Area.all
     @brand = Brand.where(external: 0)
-    @sales = Penjualan::Sale.export_sales_report(params[:start_date], params[:end_date]) if params[:start_date].present? && params[:end_date].present?
+    @sales = Penjualan::Sale.export_sales_report(params[:start_date], params[:end_date], params[:areas]) if params[:start_date].present? && params[:end_date].present?
 
     respond_to do |format|
       format.html
