6354f10314a01f77f9bd363484bc0884fa1dab0a
adjidedjo <adji.el.djo@gmail.com>
2018-12-20 09:06:04 +0700

    add export pbj

--- app/controllers/order/bekasi/bekasi_elites_controller.rb
+++ app/controllers/order/bekasi/bekasi_elites_controller.rb
@@ -7,6 +7,13 @@ class Order::Bekasi::BekasiElitesController < ApplicationController
     @branch = "BEKASI"
     @brand = "ELITE"
     render template: "order/template_order/order"
+    @pbj = SalesOrder::Order.generate_pbj(initialize_brach_id, initialize_brand) if params["format"] == "xlsx"
+    
+    respond_to do |format|
+      format.html {render template: "order/template_order/order"}
+      format.xlsx {render template: "order/template_order/pbj", 
+        :xlsx => "pbj", :filename => "pbj #{initialize_brach_id}#{initialize_brand}.xlsx"}
+    end
   end
 
   private
