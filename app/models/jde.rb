5db91c18bd1c7db81da617242da6284b486c5fb7
adjidedjo <adji.el.djo@gmail.com>
2019-02-13 11:15:16 +0700

    add planner to forecast

--- app/models/jde.rb
+++ app/models/jde.rb
@@ -1,4 +1,11 @@
 class Jde < ActiveRecord::Base
   establish_connection :jdeoracle
   self.table_name = "PRODDTA.F0006"
+  def self.get_customer_rkb(customer)
+    find_by_sql("SELECT ABALPH FROM PRODDTA.F0101 WHERE ABAN8 LIKE '%#{customer}%' AND ABAT1 = 'C'")
+  end
+
+  def self.get_sales_rkb(sales)
+    find_by_sql("SELECT ABALPH FROM PRODDTA.F0101 WHERE ABAN8 LIKE '%#{sales}%' AND ABAT1 = 'E'").first
+  end
 end
