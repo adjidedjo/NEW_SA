13d211ba82cbfc63f05254cf0991774ee29195db
adjidedjo <adji.el.djo@gmail.com>
2019-03-28 15:12:00 +0700

    fix sales homepage

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,5 @@
 class PagesController < ApplicationController
   layout 'pages'
-  before_action :revenue_sales, only: [:template]
 
   def login
   end
@@ -16,6 +15,7 @@ class PagesController < ApplicationController
 
   # set another layout for a specific action
   def template
+    @sales_month = Penjualan::SalesmanSales.revenue_sales(current_user) if current_user.present? && current_user.position == 'sales'
     render :layout => 'application'
   end
 
