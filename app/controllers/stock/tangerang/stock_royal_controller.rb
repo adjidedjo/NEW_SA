ec5727a419dda19a6c8066e7b30779f394a48bd2
adjidedjo <adji.el.djo@gmail.com>
2019-04-15 08:47:37 +0700

    fix royal stock tangerang

--- app/controllers/stock/tangerang/stock_royal_controller.rb
+++ app/controllers/stock/tangerang/stock_royal_controller.rb
@@ -47,7 +47,7 @@ class Stock::Tangerang::StockRoyalController < ApplicationController
   end
   
   def set_branch_plant
-    @branch_plant = "18151"
+    @branch_plant = "15152"
     @branch = "TANGERANG"
   end
 end
