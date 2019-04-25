c5acf6506824844c7c6865b29a495c33cbbc4a6b
adjidedjo <adji.el.djo@gmail.com>
2019-04-25 09:17:23 +0700

    fix upload sales targets

--- app/models/sales_target.rb
+++ app/models/sales_target.rb
@@ -5,18 +5,18 @@ class SalesTarget < ApplicationRecord
     header = spreadsheet.row(1)
     (2..spreadsheet.last_row).each do |i|
       row = Hash[[header, spreadsheet.row(i)].transpose]
-      target = find_by(brand: row["brand"], address_number: row["address_number"].to_i, branch: row["branch"],
-      month: row["month"], year: row["year"]) || new
-      unless row["amount"].nil? || row["amount"] == 0
-        if target.id.nil?
-          sales_name = Jde.get_sales_rkb(row["address_number"].to_i)
-          row["name"] = sales_name.nil? ? ' ' : sales_name.abalph.strip
-        target.attributes = row.to_hash
-        else
-          target["amount"] = row["amount"]
-        end
-      target.save!
+      target = find_by(brand: row["brand"], address_number: row["address_number"].to_i, branch: row["branch"].to_i,
+      month: row["month"].to_i, year: row["year"].to_i) || new
+      if target.id.nil?
+        sales_name = Jde.get_sales_rkb(row["address_number"].to_i)
+        row["name"] = sales_name.nil? ? ' ' : sales_name.abalph.strip
+        row["address_number"] = row["address_number"].to_i
+        row["amount"] = row["amount"].to_i
+      target.attributes = row.to_hash
+      else
+        target["amount"] = row["amount"].to_i
       end
+      target.save!
     end
   end
 end
