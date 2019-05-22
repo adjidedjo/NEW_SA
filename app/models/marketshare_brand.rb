47cddecfda4481e73c09b15589872f85c1c375ea
adjidedjo <adji.el.djo@gmail.com>
2019-05-22 11:08:05 +0700

    fix report marketshares

--- app/models/marketshare_brand.rb
+++ app/models/marketshare_brand.rb
@@ -1,9 +1,11 @@
 class MarketshareBrand < ActiveRecord::Base
   belongs_to :marketshare,  inverse_of: :marketshares
-  def self.customers(area, brand)
+  def self.customers(area, brand, user)
     find_by_sql("
       SELECT * FROM marketshare_brands ms WHERE
-      ms.created_at >= '#{1.months.ago.to_date}' AND ms.area_id = '#{area}' and ms.internal_brand = '#{brand}'
+      ms.created_at >= '#{1.months.ago.to_date}' AND ms.area_id = '#{area}'
+      and
+      IF('#{user}' = 'admin', ms.internal_brand = 0, ms.internal_brand = '#{brand}')
       GROUP BY ms.name, ms.customer_name;
     ")
   end
