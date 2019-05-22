73e04d16e256eb540f681bbea597686a269523b5
adjidedjo <adji.el.djo@gmail.com>
2019-05-22 10:41:30 +0700

    fix query for pbjm

--- app/models/jde.rb
+++ app/models/jde.rb
@@ -1,7 +1,6 @@
 class Jde < ActiveRecord::Base
   establish_connection :jdeoracle
   self.table_name = "PRODDTA.F0006"
-  
   def self.calculate_pbjm(from, to)
     find_by_sql("
       SELECT PF.SDMCU, PF.PBJ, PF.SDSHAN, PF.DES, SUM(PF.TOTAL_PBJM) AS TOTAL, TRIM(PF.WC) AS WC, PF.STAT FROM(
@@ -9,11 +8,12 @@ class Jde < ActiveRecord::Base
         MAX(PBJ.IBSRP6) AS WC, (CASE WHEN PBJ.SDLTTR = 580 THEN 'SENT' ELSE 'OUTS' END) AS STAT FROM (
           SELECT ORD.*, IBC.IBSRP6 FROM(
             SELECT * FROM PRODDTA.F4211
-            WHERE SDDRQJ BETWEEN '#{date_to_julian(from)}' AND '#{date_to_julian(to)}' AND SDSRP1 != 'K' AND SDLTTR != '980' AND SDDCTO IN ('SK', 'ST')
+            WHERE SDDRQJ BETWEEN '#{date_to_julian(from)}' AND '#{date_to_julian(to)}' AND SDSRP1 != 'K'
+            AND SDLTTR != '980' AND SDDCTO IN ('SK', 'ST') AND SDPRP4 != 'RM'
             ) ORD
             LEFT OUTER JOIN
             (
-              SELECT IBITM, IBSRP6 FROM PRODDTA.F4102 WHERE REGEXP_LIKE(IBMCU, '11001|11002')
+              SELECT IBITM, IBSRP6 FROM PRODDTA.F4102 WHERE REGEXP_LIKE(IBMCU, '11001|11002|12001|15151')
               AND IBSRP6 != ' ' GROUP BY IBITM, IBSRP6
             ) IBC ON IBC.IBITM = ORD.SDITM AND ROWNUM = 1
         ) PBJ LEFT JOIN
@@ -27,7 +27,7 @@ class Jde < ActiveRecord::Base
       GROUP BY PF.SDMCU, PF.PBJ, PF.SDSHAN, PF.DES, PF.WC, PF.STAT ORDER BY PF.SDSHAN
     ")
   end
-  
+
   def self.get_customer_rkb(customer)
     find_by_sql("SELECT ABALPH FROM PRODDTA.F0101 WHERE ABAN8 LIKE '%#{customer}%' AND ABAT1 = 'C'")
   end
@@ -35,8 +35,6 @@ class Jde < ActiveRecord::Base
   def self.get_sales_rkb(sales)
     find_by_sql("SELECT ABALPH FROM PRODDTA.F0101 WHERE ABAN8 LIKE '%#{sales}%' AND ABAT1 = 'E'").first
   end
-  
-  
 
   private
 
