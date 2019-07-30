34dde594917e698582b7329e918f9c45fb259fa8
adjidedjo <adji.el.djo@gmail.com>
2019-07-30 10:51:32 +0700

    add user audit

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,12 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable, :timeoutable
   validates :auth_token, uniqueness: true
+  
+  def update_tracked_fields!(request)
+    old_signin = self.last_sign_in_at
+    super
+    if self.last_sign_in_at != old_signin
+      UserAudit.create :user_id => self.id, :nama => self.nama, :action => "login"
+    end
+  end
 end
