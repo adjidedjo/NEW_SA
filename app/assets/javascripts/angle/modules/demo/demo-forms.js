2e59740fa4f201c4e376c8a34414185bad30684f
adjidedjo <adji.el.djo@gmail.com>
2020-02-21 08:46:19 +0700

    fix pbjm form

--- app/assets/javascripts/angle/modules/demo/demo-forms.js
+++ app/assets/javascripts/angle/modules/demo/demo-forms.js
@@ -79,6 +79,32 @@
                 },
                 format : 'YYYY-MM-DD'
             }).attr('readonly', 'true');
+            $('#dateffore2').datetimepicker({
+                icons : {
+                    time : 'fa fa-clock-o',
+                    date : 'fa fa-calendar',
+                    up : 'fa fa-chevron-up',
+                    down : 'fa fa-chevron-down',
+                    previous : 'fa fa-chevron-left',
+                    next : 'fa fa-chevron-right',
+                    today : 'fa fa-crosshairs',
+                    clear : 'fa fa-trash'
+                },
+                format : 'YYYY-MM-DD'
+            }).attr('readonly', 'true');
+            $('#datetfore2').datetimepicker({
+                icons : {
+                    time : 'fa fa-clock-o',
+                    date : 'fa fa-calendar',
+                    up : 'fa fa-chevron-up',
+                    down : 'fa fa-chevron-down',
+                    previous : 'fa fa-chevron-left',
+                    next : 'fa fa-chevron-right',
+                    today : 'fa fa-crosshairs',
+                    clear : 'fa fa-trash'
+                },
+                format : 'YYYY-MM-DD'
+            }).attr('readonly', 'true');
             $('#datepicker1').datetimepicker({
                 icons : {
                     time : 'fa fa-clock-o',
