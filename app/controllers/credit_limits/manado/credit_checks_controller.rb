53af9db17b6bbe807d04d5ca349027165fd39b38
adjidedjo <adji.el.djo@gmail.com>
2021-05-19 16:54:41 +0700

    fix

---
+++ app/controllers/credit_limits/manado/credit_checks_controller.rb
@@ -0,0 +1,27 @@
+class CreditLimits::Manado::CreditChecksController < ApplicationController
+  include RolesHelper
+  before_action :initialize_brand, :initialize_brach_id, :authorize_user
+  
+  def credit_checks
+    @credit_checks = CustomerLimit.find_by_sql("
+      SELECT * FROM customer_limits WHERE area_id = '#{initialize_brach_id}' AND amount_due IS NOT NULL
+    ")
+    @branch_name = Area.find(initialize_brach_id).area
+    render template: "credit_limits/templates/credit_check"
+  end
+
+  private
+
+  def initialize_brand
+    "ELITE"
+  end
+
+  def initialize_brach_id
+    26
+  end
+
+  def authorize_user
+    render template: "pages/notfound" unless general_manager(current_user) || nsm_customers(current_user) ||
+    bm_customers(current_user, initialize_brach_id) || sales_credit_limit(current_user, current_user.branch1, current_user.branch2)
+  end
+end
