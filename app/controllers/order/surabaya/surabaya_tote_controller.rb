4f1b4b975a71a7ed40670f76b382f6ed280b5506
adjidedjo <adji.el.djo@gmail.com>
2022-01-12 08:52:05 +0700

    fix

---
+++ app/controllers/order/surabaya/surabaya_tote_controller.rb
@@ -0,0 +1,59 @@
+class Order::Surabaya::SurabayaToteController < ApplicationController
+  before_action :initialize_brand, :initialize_brach_id
+  before_action :order_daily
+  
+  def order
+    @branch = "SURABAYA"
+    @brand = "TOTE"
+    @pbj = SalesOrder::Order.generate_pbj(initialize_brach_id, initialize_brand) if params["format"] == "xlsx"
+    
+    respond_to do |format|
+      format.html {render template: "order/template_order/order"}
+      format.xlsx {render template: "order/template_order/pbj", 
+        :xlsx => "pbj", :filename => "pbj #{initialize_brach_id}#{initialize_brand}.xlsx"}
+    end
+  end
+  
+  def order_normal
+    @out_daily = SalesOrder::Order.outstand_order(initialize_brach_id, initialize_brand, current_user.position, current_user.address_number.nil? ? '0' : current_user.address_number)
+    @pbj = SalesOrder::Order.generate_pbj(initialize_brach_id, initialize_brand) if params["format"] == "xlsx"
+
+    respond_to do |format|
+      format.html {render template: "order/template_order/order"}
+      format.xlsx {render template: "order/template_order/pbj",
+        :xlsx => "pbj", :filename => "pbj #{initialize_brach_id}#{initialize_brand}.xlsx"}
+    end
+  end
+  
+  def order_display
+    @out_daily = SalesOrder::Order.outstand_order(initialize_brach_id  + "D", initialize_brand, current_user.position, current_user.address_number.nil? ? '0' : current_user.address_number)
+    @pbj = SalesOrder::Order.generate_pbj(initialize_brach_id  + "D", initialize_brand) if params["format"] == "xlsx"
+
+    respond_to do |format|
+      format.html {render template: "order/template_order/order"}
+      format.xlsx {render template: "order/template_order/pbj",
+        :xlsx => "pbj", :filename => "pbj #{initialize_brach_id}#{initialize_brand}.xlsx"}
+    end
+  end
+  
+  def order_clearence
+    @out_daily = SalesOrder::Order.outstand_order(initialize_brach_id  + "C", initialize_brand, current_user.position, current_user.address_number.nil? ? '0' : current_user.address_number)
+    @pbj = SalesOrder::Order.generate_pbj(initialize_brach_id  + "C", initialize_brand) if params["format"] == "xlsx"
+
+    respond_to do |format|
+      format.html {render template: "order/template_order/order"}
+      format.xlsx {render template: "order/template_order/pbj",
+        :xlsx => "pbj", :filename => "pbj #{initialize_brach_id}#{initialize_brand}.xlsx"}
+    end
+  end
+
+  private
+
+  def initialize_brand
+    "O"
+  end
+
+  def initialize_brach_id
+    "18061"
+  end
+end
