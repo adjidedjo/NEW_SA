87425d298e739e6b372c3d09c610ba712da44056
adjidedjo <adji.el.djo@gmail.com>
2022-12-27 13:57:13 +0700

    upload forecast

--- app/models/forecast.rb
+++ app/models/forecast.rb
@@ -375,21 +375,25 @@ class Forecast < ActiveRecord::Base
     header = spreadsheet.row(1)
     (2..spreadsheet.last_row).each do |i|
       row = Hash[[header, spreadsheet.row(i)].transpose]
-      forecast = find_by(brand: row["brand"], address_number: row["address_number"].to_i, item_number: row["item_number"].strip, branch: row["branch"],
-      month: row["month"], year: row["year"], week: row["week"]) || new
+      forecast = find_by(brand: row["brand"], address_number: row["address_number"].to_i, item_number: row["item_number"], branch: row["branch"], month: row["month"], year: row["year"], week: row["week"]) || new
       unless row["quantity"].nil? || row["quantity"] == 0
         if forecast.id.nil?
           item = JdeItemMaster.get_desc_forecast(row["item_number"])
           sales_name = Jde.get_sales_rkb(row["address_number"].to_i)
+          gudang = Gudang.find_by_code(row["branch"])
           row["segment1"] = item.nil? ? 0 : item.imseg1.strip
           row["segment2"] = item.nil? ? 0 : item.imseg2.strip
           row["segment3"] = item.nil? ? 0 : item.imseg3.strip
           row["segment2_name"] = item.nil? ? 0 : JdeUdc.artikel_udc(item.imseg2.strip)
           row["segment3_name"] = item.nil? ? 0 : JdeUdc.kain_udc(item.imseg3.strip)
           row["size"] = item.nil? ? 0 : item.imseg6.strip
-          row["description"] = item.nil? ? 'UNLISTED ITEM NUMBER' : (item.imdsc1.strip + ' ' + item.imdsc2.strip)
+          row["description"] = item.nil? ? 'UNLISTED ITEM NUMBER' : ((item.imdsc1.nil? ? '' : item.imdsc1.strip) + ' ' + (item.imdsc2.nil? ? '' : item.imdsc2.strip))
           row["planner"] = '-'
           row["sales_name"] = sales_name.nil? ? ' ' : sales_name.abalph.strip
+          row["branch"] = gudang.area_id
+          row["gudang_id"] = gudang.code
+          row["gudang"] = gudang.description
+          row["sisa"] = 0
         forecast.attributes = row.to_hash
         else
           forecast["quantity"] = row["quantity"]
